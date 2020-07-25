a712d0db502a727121c5c60082f7164c42c45da0
Caroline Goulart <goulartscaroline@gmail.com>
2020-07-25 20:12:39 -0300

    Front-end with ReactJS Challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    // vamos aprender depois a usar async await com useEffect
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    // envia requisição post com os dados, e salva em response
+    // atualiza repositories
+    const response = await api.post('/repositories', {
+      title: "Repositório teste"
+    });
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const url = "/repositories/" + id;
+    api.delete(url);
+    const repositoriesUpdate = repositories.filter(repository => repository.id != id);
+    setRepositories(repositoriesUpdate);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {repositories.map(repository => 
+            <li key={repository.id}>{repository.title} 
+            <button onClick={() => handleRemoveRepository("123")}>Remover</button></li>
+          )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
